81fb5e77242d6161bb0d38749eb2d2dbcce45005
Maaj <maajb1122@gmail.com>
2024-12-16 21:25:56 +0530

    committed

--- newsblog/src/components/PremiumLayout.jsx
+++ newsblog/src/components/PremiumLayout.jsx
@@ -23,7 +23,7 @@ const PremiumLayout = () => {
   
   return (
     <>
-   <div className='flex justify-between w-[80%] mx-auto'>
+   <div className='flex justify-between w-[90%] mx-auto '>
      <PremiumNav/>
         <Outlet />
       <RigthBar otherUsers={ otherUsers}/>
